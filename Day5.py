ce346ef17f02054adac98642feebcdddba02f8d8
Saubhatta <42058066+Saubhatta@users.noreply.github.com>
2018-11-16 13:37:13 +0530

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def test(n):
+    for i in range(1,11):
+        print ( str(n) +" x "+ str(i) + " = " + str(n*i) )
+
+if __name__ == '__main__':
+    n = int(input())
+
+    test(n)
